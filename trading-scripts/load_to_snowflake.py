5c9de5b0a0f8406e5f5e714b39913aa796b0c5c2
Ernest V <ernest.viol@gmail.com>
2026-07-03 16:38:51 -0700

    feat: Calc rolling avg and std
    Created a second table to stage OHLCV before merging into raw data

--- trading-scripts/load_to_snowflake.py
+++ trading-scripts/load_to_snowflake.py
@@ -21,14 +21,40 @@ def load_to_snowflake(csv_path: Path):
     put_result = cs.execute(
         f"PUT file://{csv_path} @raw_prices_stage AUTO_COMPRESS=TRUE"
     )
-    # print("Put result:", put_result.fetchall())
 
     copy_result = cs.execute(f"""
-               COPY INTO RAW_PRICES
+               COPY INTO RAW_PRICES_STAGING
                FROM @raw_prices_stage
                MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
                PATTERN = '.*{csv_path.name}.*'
                ON_ERROR = 'CONTINUE'
                """)
 
-    # print("Copy result:", copy_result.fetchall())
+    cs.execute("""
+            MERGE INTO RAW_PRICES AS target
+            USING RAW_PRICES_STAGING AS source
+            on target.symbol = source.symbol AND target.timestamp = source.timestamp
+            WHEN NOT MATCHED THEN
+                INSERT(
+                    SYMBOL,
+                    TIMESTAMP,
+                    "OPEN",
+                    HIGH,
+                    LOW,
+                    "CLOSE",
+                    "VOLUME",
+                    TRADE_COUNT,
+                    VWAP)
+                VALUES(
+                    source.SYMBOL,
+                    source.TIMESTAMP,
+                    source.OPEN,
+                    source.HIGH,
+                    source.LOW,
+                    source.CLOSE,
+                    source.VOLUME,
+                    source.TRADE_COUNT,
+                    source.VWAP);
+            """)
+
+    cs.execute("TRUNCATE TABLE RAW_PRICES_STAGING;")
--- trading-scripts/main.py
+++ trading-scripts/main.py
@@ -1,6 +1,13 @@
 from gather_historicals import gather_historicals
 from load_to_snowflake import load_to_snowflake
 from datetime import datetime
+from stats.moving_avg import moving_avg
+
+
+def step_1(tickers, start, end):
+    for ticker in tickers:
+        csv_path = gather_historicals(ticker, start, end)
+        load_to_snowflake(csv_path)
 
 
 def main():
@@ -8,9 +15,23 @@ def main():
     start = datetime(2023, 1, 1)
     end = datetime.now()
 
-    for ticker in tickers:
-        csv_path = gather_historicals(ticker, start, end)
-        load_to_snowflake(csv_path)
+    step_1(tickers, start, end)
+    moving_avg("AAPL", 20)
+
+
+#     - [ ] Write script to compute rolling mean + stddev per ticker
+# - [ ] Implement mean-reversion signal (buy below threshold, sell above threshold)
+# - [ ] Parameterize `strategy` argument (even if only `mean_reversion` is implemented now)
+# - [ ] Generate `raw_trades` output with `strategy_used`, ticker, date, side, quantity, price
+# - [ ] Load `raw_trades` into Snowflake
+
+# compute moving avg and stddev
+
+# mean reversion signal buy sell
+
+# generate raw_trades
+
+# load raw_trades
 
 
 if __name__ == "__main__":
