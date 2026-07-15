08bedde3aa6b91d5adabfbd7fcca5dc5f51b01fd
Ernest V <ernest.viol@gmail.com>
2026-07-14 21:28:59 -0700

    feat: Filter for ticker on holdings comparison

--- streamlit/views/comparison_chart.py
+++ streamlit/views/comparison_chart.py
@@ -6,7 +6,8 @@ st.write("Comparison of strategies per ticker based on a $10,000 initial account
 st.write("Total value includes the combined cash amount and shares market value.")
 
 available_tickers = get_tickers()
-ticker = st.selectbox("Ticker", options=available_tickers.values.tolist())
+ticker = st.selectbox("Ticker", options=available_tickers["TICKER"].tolist())
+
 if ticker:
     data = get_comparison_chart(ticker)
     st.line_chart(
--- streamlit/views/holdings.py
+++ streamlit/views/holdings.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from queries import get_holdings
+from queries import get_holdings, get_tickers
 
 st.title("Holdings")
 st.write("Most current holdings by strategy and ticker")
@@ -7,6 +7,13 @@ st.write("Most current holdings by strategy and ticker")
 data = get_holdings()
 
 data = data.drop(columns=["PRICE_DATE", "RN"])
+
+
+available_tickers = get_tickers()["TICKER"].tolist()
+ticker = st.selectbox("Ticker", options=["All", *available_tickers])
+
+if ticker != "All":
+    data = data[data["TICKER"] == ticker]
 
 melted = data.melt(
     id_vars=["TICKER", "STRATEGY_USED"],
